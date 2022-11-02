fafff14caee9a6e9fa6c0b833385e752dc02a472
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2022-11-02 16:28:46 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
-*
-* Name: Nozomi Tsuchiya   Student ID: 154666192   Date: 2022-06-13
-*
 ********************************************************************************/ 
 
 import React from 'react';
